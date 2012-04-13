90e9826b29cd78de6fc18f4d846237c6c3ea4fc0
gael.duplessix <gael.duplessix@gmail.com>
2012-04-13 16:45:48 +0000

    added singleton and bufferManager files

---
+++ src/MeshBufferManager.hpp
@@ -0,0 +1,34 @@
+//
+// MeshBufferManager.hpp for  in /home/jochau_g//dev/opengl/gl-engine-42
+// 
+// Made by gael jochaud-du-plessix
+// Login   <jochau_g@epitech.net>
+// 
+// Started on  Fri Apr 13 17:10:24 2012 gael jochaud-du-plessix
+// Last update Fri Apr 13 17:12:43 2012 gael jochaud-du-plessix
+//
+
+#ifndef _MESH_BUFFER_MANAGER_HPP_
+# define _MESH_BUFFER_MANAGER_HPP_
+
+# include <BufferManager.hpp>
+
+namespace gle {
+  class MeshBufferManager : public BufferManager<MeshBufferManager, GLfloat>
+  {
+    friend class Singleton<MeshBufferManager>;
+
+  private:
+    MeshBufferManager()
+      : BufferManager()
+    {
+      setStorageBuffer(new fBuffer(fBuffer::VertexArray,
+                                   fBuffer::StaticDraw));
+    }
+    ~MeshBufferManager() {}
+
+  public:
+  };
+}
+
+# endif
