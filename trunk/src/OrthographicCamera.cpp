bd512bc5520e62fc5529f1b7048b4bbc73b483fb
loick.michard@gmail.com <loick.michard@gmail.com@d09fb794-43d3-0471-11be-b1f57cc248f8>
2012-05-25 14:01:38 +0000

    Add files

---
+++ trunk/src/OrthographicCamera.cpp
@@ -0,0 +1,40 @@
+//
+// OrthographicCamera.cpp for gle in /home/michar_l//glEngine
+// 
+// Made by loick michard
+// Login   <michar_l@epitech.net>
+// 
+// Started on  Sat Feb 18 12:53:42 2012 loick michard
+// Last update Fri May 25 15:47:46 2012 loick michard
+//
+
+#include <OrthographicCamera.hpp>
+
+gle::OrthographicCamera::OrthographicCamera(gle::Vector3<GLfloat> const& position,
+					    GLfloat near, GLfloat far,
+					    GLfloat left, GLfloat right,
+					    GLfloat bottom, GLfloat top)
+  : gle::Camera(position, position),
+    _near(near), _far(far), _left(left),
+    _right(right), _bottom(bottom), _top(top)
+{
+  _target += gle::Vector3<GLfloat>(0, 0, 1);
+  this->updateMatrix();
+}
+
+gle::OrthographicCamera::OrthographicCamera(gle::Vector3<GLfloat> const& position,
+					    gle::Vector3<GLfloat> const& target,
+					    GLfloat near, GLfloat far,
+					    GLfloat left, GLfloat right,
+					    GLfloat bottom, GLfloat top)
+  : gle::Camera(position, target),
+    _near(near), _far(far), _left(left),
+    _right(right), _bottom(bottom), _top(top)
+{
+  this->updateMatrix();
+}
+
+void gle::OrthographicCamera::update()
+{
+  _projectionMatrix = gle::Matrix4<GLfloat>::orthographic(_near, _far, _left, _right, _bottom, _top);
+}
